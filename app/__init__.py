834f1622a628867558190dd1e539f19add1daceb
LucasMCast <lucas.martins.c03@gmail.com>
2020-06-10 00:51:02 -0300

    First Commit

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from config import config
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    #associar rotas e paginas de erro personalizadas aqui
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
